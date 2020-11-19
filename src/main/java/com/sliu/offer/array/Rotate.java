061b6b2d3aa8392c882ececc2769efe9e146abb1
tristanliushuai <tristanliushuai@didiglobal.com>
2020-11-19 15:10:34 +0800

    zero

--- src/main/java/com/sliu/offer/array/Rotate.java
+++ src/main/java/com/sliu/offer/array/Rotate.java
@@ -3,28 +3,40 @@ package com.sliu.offer.array;
 import java.util.Arrays;
 
 /**
- *
  * @author: sliu
  * @date: 2020/4/24 0024 17:51
  **/
 public class Rotate {
     /**
-     给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
-     输入: [1,2,3,4,5,6,7] 和 k = 3
-     输出: [5,6,7,1,2,3,4]
-     解释:
-     向右旋转 1 步: [7,1,2,3,4,5,6]
-     向右旋转 2 步: [6,7,1,2,3,4,5]
-     向右旋转 3 步: [5,6,7,1,2,3,4]
-     尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
-     要求使用空间复杂度为 O(1) 的 原地 算法。
+     * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+     * 输入: [1,2,3,4,5,6,7] 和 k = 3
+     * 输出: [5,6,7,1,2,3,4]
+     * 解释:
+     * 向右旋转 1 步: [7,1,2,3,4,5,6]
+     * 向右旋转 2 步: [6,7,1,2,3,4,5]
+     * 向右旋转 3 步: [5,6,7,1,2,3,4]
+     * 尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+     * 要求使用空间复杂度为 O(1) 的 原地 算法。
      */
     public static void rotate() {
 
 
     }
 
+    public static void moveZeroes(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[count++] = nums[i];
+            }
+        }
+        for (int i = count; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+
+    }
+
     public static void main(String[] args) {
-        rotate();
+        moveZeroes(new int[]{0,1,0,3,12});
     }
 }
